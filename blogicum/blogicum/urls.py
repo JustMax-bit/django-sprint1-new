71e9dab7cb5421f8a51bc7ff44490a0238ead1f9
JustMax-bit <prtmax128@gmail.com>
2026-08-10 18:08:30 +0300

    commit

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+app_name = 'blogicum'
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls")),
