7c6238de1d9407795b2bb43b7ecd7695b888ecc9
JustMax-bit <prtmax128@gmail.com>
2026-08-10 16:07:23 +0300

    commit

---
+++ blogicum/blog/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+app_name = "blog"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("post/<int:num>/", views.post_detail, name="post_detail"),
+    path("category/<slug:category_slug>/", views.category_posts, name="category_posts"),
+]
