217741d3a04790d14b98b2cf917c458deb2f200e
JustMax-bit <prtmax128@gmail.com>
2026-08-14 10:55:55 +0300

    commit

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -6,7 +6,5 @@ app_name = "blogicum"
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls", namespace='blog')),
-    path("posts/", include("blog.urls", namespace='blog')),
-    path("category/", include("blog.urls", namespace='blog')),
     path("pages/", include("pages.urls", namespace='pages')),
 ]
